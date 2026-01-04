c676e23334671371039d6ee4332d5b931ec60c90
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-01-04 22:37:45 +0530

    Print type of variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+age = 19                   #for integer value 
+old = False
+old2 = True
+salary = 99.9900
+name = "Gaurav Prajapati"
+a = None
+print(type(age))
+print(type(old))
+print(type(old2))
+print(type(salary))
+print(type(name))
+print(type(a))
