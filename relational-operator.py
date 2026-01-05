ebbf43d73fff6d819e2915e17d17d55667e5e5e6
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-01-05 16:48:17 +0530

    Types of operators

---
+++ relational-operator.py
@@ -0,0 +1,9 @@
+# Relational operators 
+a = 10 
+b = 20
+print("1. a is equal to b :",a == b) # a is equal to b 
+print("2. a is not equal to b :",a != b) # a is not equal to b
+print("3. a is greater than b ",a > b ) # a is greater than b 
+print("4. a is less than b :", a < b) # a is less than b 
+print("5. a is greater are equal to b :",a >= b ) # a is greater than b 
+print("6. a is less are equal to   b :", a <= b) # a is less than b 
