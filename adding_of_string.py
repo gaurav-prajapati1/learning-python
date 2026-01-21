e1a38f01f289ba85a14d4933bcb686261766e73f
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-01-21 11:37:01 +0530

    uses of string function

---
+++ adding_of_string.py
@@ -0,0 +1,11 @@
+str1= "Gaurav"
+str2 = "Prajapati"
+print("This is first string = ",str1)
+print("This is second string = ",str2)
+final_string = str1 + str2
+print("Addition of two strings = ",final_string)
+# For find the length of string to use this function >>>> len(name_of_string)
+print("The length of first string = ",len(str1))
+print("The length of second string = ",len(str2))
+print("The result of concatenating the strings is = ",len(final_string))
+print(str1.endswith("v")) #if my string is end with "v" then its function return the output True. else the answer is False
