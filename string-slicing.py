0c414fcb25a5a9ec157d7b7232068c1460f62330
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-01-30 22:04:09 +0530

    Opeartion on string

---
+++ string-slicing.py
@@ -0,0 +1,9 @@
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:4])# including 0 but not 4
+print(fruit[1:4])# including 0 but not 4
+print(fruit[:5])
+print(fruit[0:-3]) # total stirng length = 5 and python calculate 5 - 3 = 2 so ot give index [0:2]
+print(fruit[0:len(fruit)-3])
+
---
+++ string.py
@@ -0,0 +1,19 @@
+name = "Gaurav"
+print(name [0])
+print(name [1])
+print(name [2])
+print(name [3])
+print(name [4])
+print(name [5],"\n")
+
+print("Let us know the for loop\n")
+for character in name:
+     print(character,)
+     
+print("\n")
+     
+     
+print("Find the length of string\n")
+
+print("The lenght os strins is :",len(name),"\n")
+     
