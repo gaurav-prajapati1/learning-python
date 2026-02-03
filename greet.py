72f6d2badf21c9dbc75df95b39ff92a1816de851
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-02-03 22:18:40 +0530

    If-else loop

---
+++ greet.py
@@ -0,0 +1,5 @@
+import time as ts
+print(ts.strftime('%H:%M:%S'))
+print(ts.strftime('%H'))
+print(ts.strftime('%M'))
+print(ts.strftime('%S'))
---
+++ if-else.py
@@ -0,0 +1,8 @@
+budget = int(input("Enter your budget :"))
+applePrice = 200
+if (applePrice <= budget):
+    print("You buy the apple")
+else :
+    print("You do not buy the apple")
+    
+    
