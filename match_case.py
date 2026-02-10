b2ad2377dcdc6b32847db2102bee0afccb8dc9fc
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-02-10 20:56:57 +0530

    match case

---
+++ match_case.py
@@ -0,0 +1,15 @@
+x = int(input("Enter your number :"))
+match x:
+    case 1:
+        print("you are very beautiful")
+    case 2:
+        print("Your are very Ugly !")
+    case 3: 
+        print("You looking average !")
+    case 4:
+        print("You looking so gorgeous")
+    case _:
+        print(x,"is wrong option","Choose write option")        
+    
+        
+            
