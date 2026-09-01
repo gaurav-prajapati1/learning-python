ccf5735c24e636e99f61783b03b8aa5552cf4514
GAurav Prajapati <gauravprajapati111@outlook.com>
2026-09-01 12:31:34 +0000

    Learn from college

---
+++ college-python-training/first.py
@@ -0,0 +1,42 @@
+
+# print("Gaurav Prajapati")
+
+
+#ADDITION OF TWO NUMBERS
+
+# a = int(input("Enter the value of A = "))
+# b = int (input("Enter the value of B = "))
+# c = a + b 
+# print ("The Sum of both number is = ", c)
+# print(type(a))       
+
+
+# SWAPPING THE NUMBER USING THIRD VARIABLE
+
+# a = 10 
+# b = 35
+# temp = a 
+# a = b 
+# b = temp
+# print ("The value of a = ",a)
+# print ("The value of b = ",b)
+
+
+
+# SWAPPING THE NUMBER WITHOUT USING THIRD VARIABLE
+
+# a = 5
+# b = 6
+# a , b = b , a
+# print ("The value of a = ",a)
+# print ("The value of b = ",b)
+
+# CHECK ALL THE AVAILABLE KEYWORDS IN PYTHON 
+
+# import keyword
+# print (keyword.kwlist)
+
+
+# a = int(input("Enter the side of Square = "))
+# b = a * a 
+# print("Area of Square: ",b)
---
+++ college-python-training/operators.py
@@ -0,0 +1,77 @@
+# OPERATORS
+# 1. Arthmatic Operator
+# (+, -, *, /, //, %, **)
+
+# a = 11
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+
+
+
+
+
+# 2. Relational Operator
+# (>, <, >=, <=, ==, !=)
+
+# a = 20
+# b = 30
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a == b)
+# print(a != b)
+
+
+
+
+
+
+# 3. Logical Operators
+
+# (AND or NOT)
+# a = 5
+# b = 7
+# print ((a>b) and (a<b))
+# print ((a<b) or (a>b))
+
+
+
+
+
+# 4. Assignment Operators
+# (=, +=, -=, *=, /=)
+
+# num = 10
+# num += 10
+# num += 10
+# print(num)
+
+# # PROGRAM TO FIND PRINCIPAL AND RATE AND TIME 
+# P = int(input("Enter the value of P = "))
+
+# R = int(input("Enter the value of R = "))
+
+# T = int(input("Enter the value of T = "))
+
+# SI = (P*R*T)/100
+
+# print(SI)
+
+
+
+
+# PROGRAM TO FIND THE PROFIT
+
+SP =  int(input("Enter the value of SP = "))
+CP =  int(input("Enter the value of CP = "))
+PROFIT = SP - CP
+print(PROFIT)
+
+
