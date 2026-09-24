263d142f3171d958ced81695ffa1b2c1e064a4cd
GAurav Prajapati <gauravprajapati111@outlook.com>
2026-09-24 08:09:03 +0000

    Add example

--- college-python-training/Types-of-inheritence/inheritence-in-OOPs.py
+++ college-python-training/Types-of-inheritence/inheritence-in-OOPs.py
@@ -4,22 +4,42 @@
 # example:
 
 
-class Car:
-    @staticmethod
-    def start():
-        print("Car Started")
+# class Car:
+#     @staticmethod
+#     def start():
+#         print("Car Started")
 
-    @staticmethod
-    def stop():
-        print("Car Stopped")
-
-
-class Toyotacar(Car):
-    def __init__(self, name):
-        self.name = name
+#     @staticmethod
+#     def stop():
+#         print("Car Stopped")
 
 
-car1 = Toyotacar("Fortuner")
-print(car1.name)
-print(car1.start())
-print(car1.stop())
+# class Toyotacar(Car):
+#     def __init__(self, name):
+#         self.name = name
+
+
+# car1 = Toyotacar("Fortuner")
+# print(car1.name)
+# print(car1.start())
+# print(car1.stop())
+
+# Example:
+
+
+class A:
+    varA = "Welcome to class A"
+
+
+class B:
+    varB = "Welcome to class B"
+
+
+class C(A, B):
+    varC = "Welcome to class C"
+
+
+C1 = C()
+print(C1.varA)
+print(C1.varB)
+print(C1.varC)
